81410e9ad9621f59b51fd8630b03ec33a460b6a7
Prajay Basu <prajay.basu@outlook.com>
2018-10-07 13:13:34 +0530

    Remove unnecssary files and change solution structure
    * Update *.gitignore to VS gitignore that supports Rider
    
    * Make everything .NET Core and .NET Standard
    
    * Remove uncessary nuspec files, latest *.csproj files support everything that nuspec does
    
    * Update folder structure to use modern .NET OSS convention

---
+++ samples/LoginDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography;
+using DataDrop;
+
+namespace LoginDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RC2CryptoServiceProvider rc2CSP = new RC2CryptoServiceProvider();
+            var database = new DataController("pssd","sblt-3hn8-sqoy18", true, true);
+            Console.WriteLine("UserName and Password demo");
+            Console.WriteLine("Enter User Name: ");
+            var uname = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            var pass = GetPWDMasked();
+            database.Insert(uname, pass);
+
+            Console.WriteLine("Now enter them");
+            while (true)
+            {
+                Console.WriteLine("Enter User Name: ");
+                uname = Console.ReadLine();
+                Console.WriteLine("Enter Password: ");
+                pass = GetPWDMasked();
+                if (database.ValueCheck(uname, pass))
+                {
+                    Console.WriteLine("Correct");
+                }
+                else { Console.WriteLine("Username or password incorrect"); }
+            }
+        }
+
+        private static string GetPWDMasked()
+        {
+            SecureString pwd = new SecureString( );
+            while ( true )
+            {
+                ConsoleKeyInfo i = Console.ReadKey( true );
+                if ( i.Key == ConsoleKey.Enter )
+                {
+                    break;
+                }
+                else if ( i.Key == ConsoleKey.Backspace )
+                {
+                    pwd.RemoveAt( pwd.Length - 1 );
+                    Console.Write( "\b \b" );
+                }
+                else
+                {
+                    pwd.AppendChar( i.KeyChar );
+                    Console.Write( "*" );
+                }
+            }
+            return pwd.ToString();
+        }
+    }
+}
